582c63361a5cbf7dec8da20c44343cb28b735c40
Matheus Freitas <msformigteen@live.com>
2023-06-28 22:59:42 -0300

    feat: finish start scene

--- src/InverseKine/Slides/Singularity/Singularity.js
+++ src/InverseKine/Slides/Singularity/Singularity.js
@@ -2,35 +2,35 @@ import React from 'react';
 import {
     Heading, Slide, Stepper
 } from 'spectacle';
-import Cotovelo from "./Assets/cotovelo.gif"
 import Ombro from "./Assets/ombro.gif"
-import Pulso from "./Assets/pulso.gif"
-import {TopCore} from "../../../Slides/Imager/Top";
+import { TopCore } from "../../../Slides/Imager/Top";
 
 const Steps = [
+    /**
     {
         title: 'Singularidade do Cotovelo',
         description: 'Em algumas situações, a projeção do movimento pode atingir 0 ou 180 graus de amplitude no cotovelo',
         src: Cotovelo,
     },
     {
-        title: 'Singularidade do Ombro',
-        description: 'Em algumas situações, um Joint pode aplicar uma rotação de 180 graus extremamente quase instantânea para preservar o vetor velocidade',
-        src: Ombro,
-    },
-    {
         title: 'Singularidade do Pulso',
         description: 'Um Joint pode aplicar uma rotação de 180 graus extremamente quase instantânea para preservar o vetor velocidade',
         src: Pulso,
+    },
+     **/
+    {
+        title: 'Singularidade do Ombro',
+        description: 'Em algumas situações, um Joint pode aplicar uma rotação de 180 graus quase instantânea para preservar o vetor velocidade',
+        src: Ombro,
     },
 ]
 
 
 const Singularity = () => {
     const defaultValue = {
-        title: 'Exemplos de Singularidades',
-        description: 'Muitas vezes a solução pode trazer situações impossíveis fisicamente de serem aplicadas',
-        src: null,
+        title: 'Singularidade do Ombro',
+        description: 'Em algumas situações, um Joint pode aplicar uma rotação de 180 graus quase instantânea para preservar o vetor velocidade',
+        src: Ombro,
     }
 
     return <Slide>
@@ -41,7 +41,7 @@ const Singularity = () => {
                         { value?.title ?? defaultValue.title }
                     </Heading>
                     <Heading margin="0px" color="primary" fontSize="15px">
-                        { value?.description }
+                        { value?.description ?? defaultValue.description }
                     </Heading>
                 </TopCore>
             )}
